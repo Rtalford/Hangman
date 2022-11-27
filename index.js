a6e70c387e4d51520ab0228129ef47c9db29dd54
Reynelle <RTalford13@gmail.com>
2022-11-27 09:25:05 -0500

    updates

--- index.js
+++ index.js
@@ -8,4 +8,35 @@ console.log(answer)
 
 //Select a word from the list (words array)
 
-//Set up answer array - 
+//Set up answer array - Need the answer array to display how many letters
+// are in the selected random word by using _ (underscore)
+
+
+// create a variable to hold the number of remaining letters "availableLetters"
+
+//create a loop to be used while there are letters left to be guessed 
+
+//Getting a guess from the player (prompting player to tell us a letter)
+
+//if player decides not to play - chose to end the game 
+
+      //exit the game 
+
+      // if the guess is not a letter 
+
+      // alert them that the guess must be a letter
+  
+// valid guess - guess is a letter 
+
+      // update the game state with the guess
+
+      // if the letter they guessed is in the word at that point or index
+
+      // update the answer array with the letter they guess at that point 
+      // or index 
+
+      // substract one from remaining letters 
+
+//notify the player the word they were trying to guess
+
+// congratulate the player 
