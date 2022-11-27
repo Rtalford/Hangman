819663ff38927b7a0c8206c7e752ed245a3fe8e2
Reynelle <RTalford13@gmail.com>
2022-11-27 08:35:36 -0500

    added first statements to index.js file

---
+++ index.js
@@ -0,0 +1,11 @@
+import prompt from "readline-sync";
+import wordBank from "./word-bank.js";
+console.log(wordBank[0])
+let answer = prompt.question("How are you doing ");
+console.log(answer)
+
+// Create an array of words to choose from. 
+
+//Select a word from the list (words array)
+
+//Set up answer array - 
